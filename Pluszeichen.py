a38e178ead0e09bcb8696a93f08e2931d31271d2
Omid <omidmash@gmail.com>
2024-11-21 16:41:31 +0100

    fixed formatting

--- Pluszeichen.py
+++ Pluszeichen.py
@@ -8,15 +8,11 @@ setSpeed(80)
 def cross():
     setPos(-2, 0)
     right(90)
-    repeat
-    4:
-    forward()
-
-
-setPos(0, -2)
-left(90)
-repeat
-4:
-forward()
+    repeat 4:
+        forward()
+    setPos(0, -2)
+    left(90)
+    repeat 4:
+        forward()
 
 cross()
