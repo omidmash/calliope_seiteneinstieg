d3a81ac54cfc383befc083d5842a330755e113a8
Omid <omidmash@gmail.com>
2024-11-21 17:07:35 +0100

    more pythonic

--- DiagonalePfeile.py
+++ DiagonalePfeile.py
@@ -1,6 +1,6 @@
 from calliope_mini import *
 
-cardinal = list('ARROW_NW ARROW_NE ARROW_SW ARROW_SE'.split())
+cardinal = 'ARROW_NW ARROW_NE ARROW_SW ARROW_SE'.split()
 
 for direction in cardinal:
     display.show(getattr(Image, direction))
